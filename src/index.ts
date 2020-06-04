b6a2ab329937e97d96b95264829397af2bde1a40
Jia Huang <iamfat@gmail.com>
2020-06-04 21:34:54 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -24,56 +24,56 @@ define.amd = {
   pico: true,
 };
 
-function load(moduleUri: string | string[]) {
-  if (typeof moduleUri === "object" && moduleUri.length) {
-    return Promise.all(moduleUri.map((u) => load(u)));
-  }
-  return new Promise((resolve, reject) => {
-    if (!global.define) {
-      global.define = define;
-    } else if (!global.define.amd || !global.define.amd.pico) {
-      reject("Incompatible mix of defines found!");
-    }
-    // Create a script for feature detection & potentially loading with.
-    const script = document.createElement("script");
-    if (typeof script.addEventListener !== "undefined") {
-      script.addEventListener("load", () => {
-        const module = loadedModules.pop();
-        if (typeof module.factory === "function") {
-          if (module.deps instanceof Array) {
-            Promise.all(
-              module.deps.map((depName) => {
-                const depModules = loadedModules.filter(
-                  ({ name }) => name === depName
-                );
-                if (depModules.length === 0) {
-                  return load(depName);
-                } else {
-                  const module = depModules[0];
-                  if (typeof module.factory === "function") {
-                    return module.factory();
+function load(...moduleUris: string[]) {
+  const promises = moduleUris.map((moduleUri) => {
+    return new Promise((resolve, reject) => {
+      if (!global.define) {
+        global.define = define;
+      } else if (!global.define.amd || !global.define.amd.pico) {
+        reject("Incompatible mix of defines found!");
+      }
+      // Create a script for feature detection & potentially loading with.
+      const script = document.createElement("script");
+      if (typeof script.addEventListener !== "undefined") {
+        script.addEventListener("load", () => {
+          const module = loadedModules.pop();
+          if (typeof module.factory === "function") {
+            if (module.deps instanceof Array) {
+              Promise.all(
+                module.deps.map((depName) => {
+                  const depModules = loadedModules.filter(
+                    ({ name }) => name === depName
+                  );
+                  if (depModules.length === 0) {
+                    return load(depName);
                   } else {
-                    return module.factory;
+                    const module = depModules[0];
+                    if (typeof module.factory === "function") {
+                      return module.factory();
+                    } else {
+                      return module.factory;
+                    }
                   }
-                }
-              })
-            ).then((depModules) => {
-              resolve(module.factory(...depModules));
-            });
+                })
+              ).then((depModules) => {
+                resolve(module.factory(...depModules));
+              });
+            } else {
+              resolve(module.factory());
+            }
           } else {
-            resolve(module.factory());
+            resolve(module.factory);
           }
-        } else {
-          resolve(module.factory);
-        }
-      });
-      script.src = moduleUri as string;
-      script.async = true;
-      document.body.appendChild(script);
-    } else {
-      reject('Your browser is too OLD to run "pico" loader');
-    }
+        });
+        script.src = moduleUri as string;
+        script.async = true;
+        document.body.appendChild(script);
+      } else {
+        reject('Your browser is too OLD to run "pico" loader');
+      }
+    });
   });
+  return promises.length > 1 ? Promise.all(promises) : promises[0];
 }
 
 const pico = { load, define };
